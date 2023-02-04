c1c01c31d7f62f8b411c7d26f6cd852f40713cdb
firatstory.eth <talefirat@gmail.com>
2023-02-05 00:18:15 +0300

    implement whitelisting and tokenURI

--- backend/contracts/IPNFTContract.sol
+++ backend/contracts/IPNFTContract.sol
@@ -5,24 +5,76 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
 contract IP_NFTContract is Ownable, ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
+    mapping(address => bool) public brightlist;
+    mapping(uint256 => ContractData) public contractData;
+
+    struct ContractData {
+        string contractData;
+        string description;
+        string name;
+    }
+
+    event NewMinter(address indexed _minter);
+    event RevokedMinter(address indexed _minter);
+    event NewToken(uint256 indexed tokenId);
+
     constructor() ERC721("IP_NFTContract", "IP-NFT") {}
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    modifier hasBrightlisted() {
+        require(brightlist[_msgSender()], "NOT_IN_WHITELIST");
+        _;
+    }
+
+    /**
+     * @notice Add to brightlist
+     */
+    function addToBrightlist(address _userAddress) public onlyOwner {
+        brightlist[_userAddress] = true;
+    }
+
+    /**
+     * @notice Remove from brightlist
+     */
+    function removeFrombrightlist(address _userAddress) public onlyOwner {
+        delete brightlist[_userAddress];
+    }
+
+    function safeMint(address _to, string memory uri) public hasBrightlisted {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
-
-        _safeMint(to, tokenId);
+        removeFrombrightlist(_to);
+        _safeMint(_to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view override(ERC721) returns (bool) {
-        return super.supportsInterface(interfaceId);
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        _requireMinted(tokenId);
+
+        ContractData memory _contractData = contractData[tokenId];
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name":',
+                        _contractData.name,
+                        '"description":',
+                        _contractData.description,
+                        '"contractData":',
+                        _contractData.contractData,
+                        '"}'
+                    )
+                )
+            )
+        );
+        return string(abi.encodePacked("data:application/json;base64,", json));
     }
 }
